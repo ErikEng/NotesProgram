904e6d75040084a953379c5888393caff7b2e117
Erik Engelhardt <engelhar@kth.se>
2016-05-22 22:01:58 +0200

    Started on command parser, but getting errors from having byte but needing []byte

--- NotesFormatter.go
+++ NotesFormatter.go
@@ -1,11 +1,15 @@
 package main
 
 import (
-	"os"
+	//"os"
 //    "bytes"
     "fmt"
     "log"
     "io/ioutil"
+    //"os/exec"
+    "regexp"
+    //"strings"
+    "time"
     //"bufio"
 )
 
@@ -19,22 +23,30 @@ func main() {
 	}
 	fmt.Print("What is the full name of your text?:")
 	fmt.Scanln(&sourceName)
-	sourceText:= ioutil.ReadFile(sourceName)
-	//d1 := []byte("<hmtl>\n<header> Subject, Date </header>\n<div> Lorem ipsum dolor sit amet. Nunc quis pellentesque nibh. Ut vitae <b>Aenean placerat </b> nisi eget iaculis facilisis.</div> \n <embed  src=\"Slides/lect1.pdf\" width=\"800px\" height=\"2100px\">")
+	sourceText, _ := ioutil.ReadFile(sourceName)
 	response += ".html"
-	d2 := []byte("momomomomo")
-	fmt.Print(response)
+	//formatedText := commandSearcher(sourceText)
+	formatedByte := commandSearcher(sourceText)
+	//fmt.Print(response)
 	//fmt.Print(d1)
+	//os.Create(response)
+    ioutil.WriteFile(response, formatedByte, 0644)
+   	//exec.Command("run", response).Run();
+}
 
-	os.Create(response)
-    ioutil.WriteFile(response, d2, 0644)
+func commandSearcher(providedText []byte) (formated []byte){
+	//words := strings.Fields(providedText) // Fields extracts the words into a slice.
+	//var formated []byte
+	for _, w := range providedText {
+		if regexp.Match(":date:", w) {
+		//replaces the codeword :date: with the current date
+		currentTime:=time.Now().Format("2006-01-02 15:04:05")
 
+		w =  []byte(currentTime)
+		}
+		//if strings.EqualFold("", w)
 
-//    ioutil.WriteFile(response, d2, 0644)
-	//err := CreatePng("picture.html")
-	//if err != nil {
-	//		log.Fatal(err)
-	//	}
-	 
-	//fmt.Println(time.Since(start))
+		append(formated, w...)
+	}
+	return formated
 }
