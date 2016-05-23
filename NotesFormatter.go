0b43aa5c4a3059bb9ebaa6b9c4ad35ae77dd6daf
dnigatu <dnigatu@kth.se>
2016-05-23 09:46:18 +0200

    Update NotesFormatter.go

--- NotesFormatter.go
+++ NotesFormatter.go
@@ -37,4 +37,13 @@ func main() {
 	//	}
 	 
 	//fmt.Println(time.Since(start))
+func splitpdf(fp, lp, filename, newname string) {
+	cmd := "pdfimages"
+	args := []string{"-j", "-f", fp, "-l", lp, filename, newname}
+	if err := exec.Command(cmd, args...).Run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Println("Successfully converted pdf %w to images %w", filename, newname)
+}
 }
