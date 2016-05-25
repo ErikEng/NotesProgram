a302d4981334cb126d28294bb0177b528a57d831
Erik Engelhardt <engelhar@kth.se>
2016-05-25 02:46:07 +0200

    Add functionality to generate htmls with titles, jpgs and dates from commands

--- NotesFormatter.go
+++ NotesFormatter.go
@@ -10,6 +10,7 @@ import (
     "time"
     "os/exec"
     "regexp"
+   //"strconv"
     //"bufio"
 )
 
@@ -44,29 +45,56 @@ func commandSearcher(providedByte []byte) (text []byte){
 	//words := strings.Fields(providedText) // Fields extracts the words into a slice.
 	//var formated []byte
 	var buffer bytes.Buffer
+
 	providedText := string(providedByte)
 	w := strings.Fields(providedText)
 	date, _ := regexp.Compile(":date:") //create regexp for date-command
 	slide, _ := regexp.Compile(":slide:")
-	for _, word := range w {
-		
+	title, _ := regexp.Compile(":title:")
+	newLine, _ := regexp.Compile(":end:")
+	buffer.WriteString("<div>")
+	n := 0
+	for n < len(w) {
+		word := w[n]
  
 		if date.MatchString(word) {
 		//if /[:date:]/ {	
 			currentTime:=time.Now().Format("2006-01-02 15:04:05")
 			//fmt.Print("success" + currentTime)
 			buffer.WriteString(currentTime)
+
 		} else if slide.MatchString(word) {
-			slideSource := "Slides/"+word[5:len(word)] //takes out the source of the slide and its name 
-			htmlExpression=("<embed  src="+slideSource+" width="800px" height="2100px">")
+			if n >= len(w) {
+				fmt.Println("No slide was given after the :slide: command")
+				break
+			}
+			buffer.WriteString("</div>")
+			slideSource := "Slides/"+w[n+1] + ".jpg"//takes out the source of the slide and its name 
+			htmlExpression := "<embed  src=\"" + slideSource + "\" width=\"1100px\" height=\"800px\">"
 			buffer.WriteString(htmlExpression) //inserts the slide into the html text
+
+		} else if title.MatchString(word) {
+			//wordsInTitle := 0
+			title := ""
+			//m := n+1 //We'll increase n so we don't get the title printed twice, so we create a new local incrementing variable
+			for n < len(w) && !newLine.MatchString(w[n+1]) {   // loop until you've encountered newline
+				title += " " + w[n+1]
+				//m++
+				n++  //increase n so title isn't looped through and printed again
+			}
+			n++ //ignore the :end: command
+			divExpression := "<h1>" + title + "</h1>"
+			buffer.WriteString(divExpression)
+			
+		
 		} else {
 			buffer.WriteString(word)
 		}
+		n++
 		buffer.WriteString(" ")
 	}
 	text = []byte(buffer.String())
-	//fmt.Println("why")
+	//fmt.Println("why") :head
 	return text
 }
 
